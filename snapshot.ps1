1413d094f5ab3f7656ff3df291f4f9bf6c7ca07d
karthik Jebamony <88184822+karnajk1987@users.noreply.github.com>
2023-03-30 21:58:33 +0530

    Create snapshot.ps1

---
+++ snapshot.ps1
@@ -0,0 +1,28 @@
+#Connect-AzAccount
+
+Select-AzSubscription -SubscriptionId "58f58984-c7ec-4128-9dcf-9b5d77697c3d"
+
+$resourceGroupName = 'webapp-dev'
+$location = 'eastus'
+$vmName = 'webapp-dev-wind-01'
+$snapshotName = 'webapp-dev-wind-01-image-backup-02'
+$resourceGroupName2 = 'webapp-qa'
+
+
+$vm = Get-AzVM `
+    -ResourceGroupName $resourceGroupName `
+    -Name $vmName
+
+
+
+$snapshot = New-AzSnapshotConfig `
+    -SourceUri $vm.StorageProfile.OsDisk.ManagedDisk.Id `
+    -Location $location `
+    -CreateOption copy
+
+
+
+New-AzSnapshot `
+    -Snapshot $snapshot `
+    -SnapshotName $snapshotName `
+    -ResourceGroupName $resourceGroupName2

81b3a96c3d9b13932978800b20ab26255ce3b0ca
karthik Jebamony <88184822+karnajk1987@users.noreply.github.com>
2023-04-03 23:30:25 +0530

    Update powershell.ps1
    Updated the script

--- powershell.ps1
+++ powershell.ps1
@@ -4,7 +4,6 @@ $vmResourceIds = @(
     "/subscriptions/58f58984-c7ec-4128-9dcf-9b5d77697c3d/resourceGroups/webapp-dev-rg/providers/Microsoft.Network/networkInterfaces/webapp-vm-01346",
     "/subscriptions/58f58984-c7ec-4128-9dcf-9b5d77697c3d/resourceGroups/webapp-dev-rg/providers/Microsoft.Compute/virtualMachines/webapp-vm-02",
     "/subscriptions/58f58984-c7ec-4128-9dcf-9b5d77697c3d/resourceGroups/webapp-dev-rg/providers/Microsoft.Network/networkInterfaces/webapp-vm-02670"
-    # add additional VM and NIC resource IDs as needed
 )
 
 # Loop through the resource IDs and delete the VMs and NICs
@@ -13,8 +12,13 @@ foreach ($resourceId in $vmResourceIds) {
     if ($resourceId.Contains("/virtualMachines/")) {
         # Extract the VM name from the resource ID
         $vmName = ($resourceId -split '/')[-1]
-        # Delete the VM
-        Remove-AzVM -ResourceGroupName webapp-dev-rg -Name $vmName -Force -AsJob
+        # Get the VM object
+        $vm = Get-AzVM -ResourceGroupName webapp-dev-rg -Name $vmName
+        # Remove the NIC from the VM
+        $nicId = $vm.NetworkProfile.NetworkInterfaces[0].Id
+        $vm = Remove-AzVMNetworkInterface -VM $vm -Id $nicId
+        # Update the VM
+        Update-AzVM -ResourceGroupName webapp-dev-rg -VM $vm -Verbose -AsJob
     } elseif ($resourceId.Contains("/networkInterfaces/")) {
         # Extract the NIC name from the resource ID
         $nicName = ($resourceId -split '/')[-1]

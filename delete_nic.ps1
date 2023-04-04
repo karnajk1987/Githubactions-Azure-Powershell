1e6d63892b72d9f9ba099b73face2fdca648e2bb
karthik Jebamony <88184822+karnajk1987@users.noreply.github.com>
2023-04-04 21:05:32 +0530

    Create delete_nic.ps1
    Updated delete_nic.ps1

---
+++ delete_nic.ps1
@@ -0,0 +1,37 @@
+# Define the resource IDs of the NICs to delete
+$nicResourceIds = @(
+    "/subscriptions/58f58984-c7ec-4128-9dcf-9b5d77697c3d/resourceGroups/webapp-prod-p01/providers/Microsoft.Network/networkInterfaces/AZ-VM-00-NIC-0",
+    "/subscriptions/58f58984-c7ec-4128-9dcf-9b5d77697c3d/resourceGroups/webapp-prod-p01/providers/Microsoft.Network/networkInterfaces/AZ-VM-00-NIC-1",
+    "/subscriptions/58f58984-c7ec-4128-9dcf-9b5d77697c3d/resourceGroups/webapp-prod-p01/providers/Microsoft.Network/networkInterfaces/AZ-VM-00-NIC-2",
+    "/subscriptions/58f58984-c7ec-4128-9dcf-9b5d77697c3d/resourceGroups/webapp-prod-p01/providers/Microsoft.Network/networkInterfaces/AZ-VM-00-NIC-3",
+    "/subscriptions/58f58984-c7ec-4128-9dcf-9b5d77697c3d/resourceGroups/webapp-prod-p01/providers/Microsoft.Network/networkInterfaces/AZ-VM-00-NIC-4"
+    # Add additional NIC resource IDs here as needed
+)
+
+foreach ($nicResourceId in $nicResourceIds) {
+    # Get the network interface
+    $nic = Get-AzNetworkInterface -ResourceId $nicResourceId
+
+    # If the network interface is associated with a virtual machine, dissociate it
+    if ($null -ne $nic.VirtualMachine) {
+        Remove-AzNetworkInterface -ResourceId $nicResourceId -Force
+    }
+
+    # Delete the network interface
+    Remove-AzNetworkInterface -ResourceId $nicResourceId -Force
+}
+
+# Define the resource IDs of the VMs to delete
+$vmResourceIds = @(
+    "/subscriptions/58f58984-c7ec-4128-9dcf-9b5d77697c3d/resourceGroups/webapp-prod-p01/providers/Microsoft.Compute/virtualMachines/AZ-VM-00-0",
+    "/subscriptions/58f58984-c7ec-4128-9dcf-9b5d77697c3d/resourceGroups/webapp-prod-p01/providers/Microsoft.Compute/virtualMachines/AZ-VM-00-1",
+    "/subscriptions/58f58984-c7ec-4128-9dcf-9b5d77697c3d/resourceGroups/webapp-prod-p01/providers/Microsoft.Compute/virtualMachines/AZ-VM-00-2",
+    "/subscriptions/58f58984-c7ec-4128-9dcf-9b5d77697c3d/resourceGroups/webapp-prod-p01/providers/Microsoft.Compute/virtualMachines/AZ-VM-00-3",
+    "/subscriptions/58f58984-c7ec-4128-9dcf-9b5d77697c3d/resourceGroups/webapp-prod-p01/providers/Microsoft.Compute/virtualMachines/AZ-VM-00-4"
+    # Add additional VM resource IDs here as needed
+)
+
+
+foreach ($vmResourceId in $vmResourceIds) {
+    Remove-AzVM -ResourceId $vmResourceId -Force
+}
